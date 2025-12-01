c9088c7c48cc9dd307fbcac4a12a81f9dd5f15a8
Boy <abc35100762@gmail.com>
2025-12-01 15:47:04 +0800

    Time: 7 ms (83.81%), Space: 10.3 MB (93.58%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
+    int change(int amount, vector<int>& coins) {    // 組合 vs. 377. 排列
         vector<unsigned long long> dp(amount + 1);
         dp[0] = 1;
-
         for (int coin : coins) {
             for (int i = coin; i <= amount; i++) {
                 dp[i] += dp[i - coin];
