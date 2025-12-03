1414991294ee443a4c6872f4dcfb420005367cd1
Boy <abc35100762@gmail.com>
2025-12-03 23:20:45 +0800

    Time: 0 ms (100%), Space: 12.5 MB (94.27%) - LeetHub

--- 0120-triangle/0120-triangle-bottom-up.cpp
+++ 0120-triangle/0120-triangle-bottom-up.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (j == 0) {
-                    triangle[i][0] += triangle[i-1][0];
-                } else {
-                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j]);
-                }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);
             }
-            triangle[i][i] += triangle[i-1][i-1];
         }
-        int res = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            res = min(res, triangle[n-1][i]);
-        }
-        return res;
+        
+        return triangle[0][0];
     }
 };
