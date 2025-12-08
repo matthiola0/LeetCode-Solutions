eb6dac5eea38e0c7954cbd3f0f14c2a75a84e70a
Boy <abc35100762@gmail.com>
2025-12-08 15:49:43 +0800

    Time: 32 ms (26.62%), Space: 23.3 MB (30.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands-bfs.cpp
+++ 0200-number-of-islands/0200-number-of-islands-bfs.cpp
@@ -2,29 +2,35 @@ class Solution {
 private:      
     int m, n;
     vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    void dfs(vector<vector<char>>& grid, pair<int, int> cur) {
-        grid[cur.first][cur.second] = '2';  // visited
-        for (int i = 0; i < 4; i++) {
-            int next_x = cur.first + dir[i].first, 
-                next_y = cur.second + dir[i].second;
-            if (next_x < 0 || next_x >= m || next_y < 0 || next_y >= n) {
-                continue;
-            }
-            if (grid[next_x][next_y] == '1') {
-                dfs(grid, {next_x, next_y});
+    void bfs(vector<vector<char>>& grid, pair<int, int> first) {
+        grid[first.first][first.second] = '2';  // visited
+        queue<pair<int, int>> q;
+        q.push(first);
+        while (!q.empty()) {
+            auto cur = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int next_x = cur.first + dir[i].first,
+                    next_y = cur.second + dir[i].second;
+                if (next_x < 0 || next_x >= m || next_y < 0 || next_y >= n) {
+                    continue;
+                }
+                if (grid[next_x][next_y] == '1') {
+                    q.push({next_x, next_y});
+                    grid[next_x][next_y] = '2';
+                }
             }
         }
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int res = 0;
-        vector<pair<int, int>> ones;
         m = grid.size();
         n = grid[0].size();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    dfs(grid, {i, j});
+                    bfs(grid, {i, j});
                     res++;
                 }
             }
@@ -32,3 +38,4 @@ public:
         return res;
     }
 };
+
