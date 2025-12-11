0a3f6da08b827c3b9e6d0565f177ec0af1d6dd1c
Boy <abc35100762@gmail.com>
2025-12-11 14:06:20 +0800

    Time: 0 ms (100%), Space: 11.7 MB (81.87%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string, unordered_map<string, double>> adj;
-        for (int i = 0; i < equations.size(); i++) {
-            adj[equations[i][0]][equations[i][1]] = values[i];
-            adj[equations[i][1]][equations[i][0]] = 1.0/values[i];
+        unordered_map<string, unordered_map<string, double>> dict;
+        vector<double> res;
+        int n = equations.size();
+        for (int i = 0; i < n; i++) {
+            dict[equations[i][0]][equations[i][1]] = values[i];
+            dict[equations[i][1]][equations[i][0]] = 1.0/values[i];
         }
 
-        vector<double> res;
-        for (auto q : queries) {
-            if (adj.find(q[0]) == adj.end() || adj.find(q[1]) == adj.end()) {
-                res.push_back(-1.0);
-                continue;
-            }
-
-            stack<pair<string, double>> stk;
+        for (auto query : queries) {
             unordered_set<string> visited;
+            queue<pair<string, double>> q;
             bool found = false;
-            stk.push({q[0], 1});
-            visited.insert(q[0]);
-
-            while(!stk.empty()) {
-                auto [cur_str, cur_val] = stk.top();
-                stk.pop();
-                if (cur_str == q[1]) {
-                    res.push_back(cur_val);
+            if (dict.count(query[0])) {
+                q.push({query[0], 1.0});
+                visited.insert(query[0]);
+            }
+            while(!q.empty()) {
+                auto [cur, val] = q.front();
+                q.pop();
+                visited.insert(cur);
+                if (cur == query[1]) {
                     found = true;
+                    res.push_back(val);
                     break;
                 }
-                for (auto next : adj[cur_str]) {
-                    if (visited.count(next.first) == 0) {
-                        visited.insert(next.first);
-                        stk.push({next.first, cur_val * next.second});
+
+                for (auto neighbor : dict[cur]) {
+                    if (!visited.count(neighbor.first)) {
+                        q.push({neighbor.first, val * neighbor.second});
                     }
                 }
             }
-            if (!found) res.push_back(-1.0);
+            if (!found) res.push_back(-1);
         }
         return res;
     }
