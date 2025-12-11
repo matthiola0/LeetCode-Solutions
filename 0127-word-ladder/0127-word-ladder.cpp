a97a7c0e770484104e324be0d5166c398ca3be4b
Boy <abc35100762@gmail.com>
2025-12-11 17:41:16 +0800

    Time: 50 ms (79.95%), Space: 19.7 MB (85.1%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> exist(wordList.begin(), wordList.end());
-        unordered_map<string, unordered_map<string, bool>> mp;
-        if (exist.find(endWord) == exist.end()) return 0;
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        if (!dict.count(endWord)) return 0;
 
-        // build edge
-        string tmp;
-        for (int i = 0; i < beginWord.size(); i++) {    // begin word
-            string tmp = beginWord;
-            for (int j = 0; j < 26; j++) {          
-                if ('a' + j != beginWord[i]) {
-                    tmp[i] = 'a' + j;
-                    if (exist.find(tmp) != exist.end()) {
-                        mp[beginWord][tmp] = true;
-                    }
-                }
-            }
-        }
-        for (string s : wordList) {                     // other
-            string tmp;
-            for (int i = 0; i < s.size(); i++) {
-                tmp = s;
-                for (int j = 0; j < 26; j++) {
-                    if ('a' + j != s[i]) {
-                        tmp[i] = 'a' + j;
-                        if (exist.find(tmp) != exist.end()) {
-                            mp[s][tmp] = true;
-                        }
-                    }
-                }
-            }
-        }
-        
-        int ans = 1;
-        unordered_set<string> visited;
         queue<string> q;
         q.push(beginWord);
-        visited.insert(beginWord);
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                string cur = q.front();
+        if (dict.count(beginWord)) dict.erase(beginWord);
+
+        int ans = 0;
+        while(!q.empty()) {
+            int t = q.size();
+            ans++;
+            while(t--) {
+                string s = q.front();
                 q.pop();
-                for (auto next : mp[cur]) {
-                    if (next.first == endWord) {
-                        return ans + 1;
+                if (s == endWord) return ans;
+
+                string tmp = s;
+                for (int i = 0; i < tmp.size(); i++) {
+                    char origin = tmp[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == origin) continue;
+                        tmp[i] = c;
+                        if (dict.count(tmp)) {
+                            q.push(tmp);
+                            dict.erase(tmp);
+                        }
                     }
-                    if (visited.find(next.first) == visited.end()) {
-                        q.push(next.first);
-                        visited.insert(next.first);
-                    }
+                    tmp[i] = origin;
                 }
             }
-            ans++;
         }
         return 0;
     }
