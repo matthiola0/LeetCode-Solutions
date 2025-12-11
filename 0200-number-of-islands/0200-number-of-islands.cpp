549bb90c561eb86f0d5b2b3e67fc2839a5034db4
Boy <abc35100762@gmail.com>
2025-12-11 15:08:31 +0800

    Time: 20 ms (93.88%), Space: 16.7 MB (58.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,28 @@
 class Solution {
-private:      
+private:
     int m, n;
-    vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    void dfs(vector<vector<char>>& grid, pair<int, int> cur) {
-        grid[cur.first][cur.second] = '2';  // visited
-        for (int i = 0; i < 4; i++) {
-            int next_x = cur.first + dir[i].first, 
-                next_y = cur.second + dir[i].second;
-            if (next_x < 0 || next_x >= m || next_y < 0 || next_y >= n) {
-                continue;
-            }
-            if (grid[next_x][next_y] == '1') {
-                dfs(grid, {next_x, next_y});
-            }
-        }
+    void bfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int x, int y) {
+        if (x < 0 || x >= m || y < 0 || y >= n || visited[x][y] || grid[x][y] == '0') return;
+        visited[x][y] = true;
+        grid[x][y] = '0';
+        bfs(grid, visited, x + 1, y);
+        bfs(grid, visited, x, y + 1);
+        bfs(grid, visited, x - 1, y);
+        bfs(grid, visited, x, y - 1);
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int res = 0;
-        vector<pair<int, int>> ones;
-        m = grid.size();
-        n = grid[0].size();
+        m = grid.size(), n = grid[0].size();
+        int ans = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, {i, j});
-                    res++;
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    bfs(grid, visited, i, j);
+                    ans++;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
