68543fd03fd7dec3fe1c96ce4ef6665f983ca49c
Boy <abc35100762@gmail.com>
2025-12-12 20:54:36 +0800

    Time: 3 ms (89.94%), Space: 23.9 MB (34.65%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    	sort(intervals.begin(), intervals.end());
         vector<vector<int>> res;
-        sort(intervals.begin(), intervals.end());
-        for (auto &i : intervals) {
-            if (res.empty() || i[0] > res.back()[1]) {
-                res.push_back(i);
+        res.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
             } else {
-                res.back()[1] = max(res.back()[1], i[1]);
+                res.push_back(intervals[i]);
             }
         }
         return res;
     }
 };
+
