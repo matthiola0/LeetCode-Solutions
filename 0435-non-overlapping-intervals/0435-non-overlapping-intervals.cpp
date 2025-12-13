3fc0afa2e5cdaf2bd1a84c307e6f0e774fe4691b
Boy <abc35100762@gmail.com>
2025-12-13 12:08:03 +0800

    Time: 88 ms (16.84%), Space: 106.7 MB (14.41%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,11 +4,11 @@ public:
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
             return a[1] < b[1];
         });
-        stack<vector<int>> stk;
+        int lastEnd = INT_MIN, cnt = 0;
         for (auto i : intervals) {
-            if (!stk.empty() && stk.top()[1] > i[0]) continue;
-            stk.push(i);
+            if (lastEnd > i[0]) cnt++;
+            else lastEnd = i[1];
         }
-        return intervals.size() - stk.size();
+        return cnt;
     }
 };
