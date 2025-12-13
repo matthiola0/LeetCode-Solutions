8ca597c6bd9db790b34f203e7c2661ddd992dedb
Boy <abc35100762@gmail.com>
2025-12-13 17:26:58 +0800

    Time: 8 ms (37.3%), Space: 20.9 MB (12.9%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indeg(numCourses);
-        vector<vector<int>> adj(numCourses);
+    	vector<int> indeg(numCourses, 0);
+        unordered_map<int, vector<int>> neighbors;
+        for (auto p : prerequisites) {
+            indeg[p[0]]++;
+            neighbors[p[1]].push_back(p[0]);
+        }
         queue<int> q;
-        int cnt = 0;
-        for (auto i : prerequisites) {
-            adj[i[1]].push_back(i[0]);
-            indeg[i[0]]++;
-        }
-        for (int i = 0; i < numCourses; i++) {
-            if (indeg[i] == 0) {
-                q.push(i);
+        for (int i = 0; i < numCourses; i++) 
+            if (indeg[i] == 0) q.push(i);
+            int finishCourses = 0;
+            while (!q.empty()) {
+                int cur = q.front();
+                q.pop();
+                finishCourses++;
+                for (int i : neighbors[cur]) {
+                    indeg[i]--;
+                    if (indeg[i] == 0) q.push(i);
             }
         }
-
-        while(!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            cnt++;
-
-            for (int next : adj[cur]) {
-                indeg[next]--;
-                if (indeg[next] == 0) {
-                    q.push(next);
-                }
-            }
-        }
-        if (cnt == numCourses) return true;
+        if (finishCourses == numCourses) return true;
         return false;
     }
 };
+
