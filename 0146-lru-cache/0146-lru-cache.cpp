dc5f0526773d14f6beeee985b41bc3cf0f0a877c
Boy <abc35100762@gmail.com>
2025-12-16 09:18:32 +0800

    Time: 66 ms (77.26%), Space: 173.1 MB (81.9%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,28 +1,26 @@
 class LRUCache {
-private:
     struct Node {
-        Node *next, *prev;
         int key, val;
-        Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr) {}
+        Node *prev, *next;
+        Node(int k, int v): key(k), val(v), prev(nullptr), next(nullptr) {}
     };
 
-    void removeNode(Node *node) {
-        node->next->prev = node->prev;
-        node->prev->next = node->next;
-    }
     void addToHead(Node *node) {
         head->next->prev = node;
         node->next = head->next;
         head->next = node;
         node->prev = head;
     }
+    void remove(Node *node) {
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
+    }
 
-    unordered_map<int, Node*> dict;
     Node *head, *tail;
     int capacity, cur_capacity;
-
+    unordered_map<int, Node*> dict;
 public:
-    LRUCache(int capacity) : capacity(capacity) {
+    LRUCache(int capacity): capacity(capacity) {
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
         head->next = tail;
@@ -31,31 +29,31 @@ public:
     }
     
     int get(int key) {
-        if (dict.count(key)) {
-            removeNode(dict[key]);
-            addToHead(dict[key]);
-            return dict[key]->val;
-        }
-        return -1;
+        if (!dict.count(key)) return -1;
+        Node *node = dict[key];
+        remove(node);
+        addToHead(node);
+        return node->val;
     }
     
     void put(int key, int value) {
-        if (dict.count(key)) {
-            dict[key]->val = value;
-            removeNode(dict[key]);
-            addToHead(dict[key]);
-        } else {
-            dict[key] = new Node(key, value);
+        if (!dict.count(key)) {
+            Node *node = new Node(key, value);
+            dict[key] = node;
             if (cur_capacity == capacity) {
                 Node *old = tail->prev;
-                removeNode(old);
+                remove(old);
                 dict.erase(old->key);
                 delete old;
-                addToHead(dict[key]);
             } else {
-                addToHead(dict[key]);
                 cur_capacity++;
             }
+            addToHead(node);
+        } else {
+            Node *node = dict[key];
+            node->val = value;
+            remove(node);
+            addToHead(node);
         }
     }
 };
