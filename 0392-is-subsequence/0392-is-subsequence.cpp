4a75c689e10ae5513f69045c1976aead11d692d7
Boy <abc35100762@gmail.com>
2025-12-27 22:46:19 +0800

    Time: 0 ms (100%), Space: 8.5 MB (65.39%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.size() > t.size()) return false;
+        if (s.size() == 0) return true;
+        int i = 0;
+        for (int j = 0; j < t.size(); j++) {
+            if (s[i] == t[j]) i++;
+            if (i == s.size()) return true;
+        }
+        return false;
+    }
+};
