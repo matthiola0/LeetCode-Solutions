c9d154f59afa981f1aaf59d26114808f16e39802
Boy <abc35100762@gmail.com>
2025-12-28 23:39:02 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.rbegin(), citations.rend());
-        int i = 0;
-        for (; i < citations.size(); i++) {
-            if (i + 1 > citations[i]) break;
+        int n = citations.size();
+        vector<int> buckets(n + 1, 0);
+
+        for (int c : citations) {
+            if (c >= n) buckets[n]++;
+            else buckets[c]++;
         }
-        return i;
+
+        int count = 0;
+        for (int i = n; i > 0; i++) {
+            count += buckets[i];
+            if (count >= i) return i;
+        }
+        return 0;
     }
 };
