6c95f492ae7b2602edebb0192ea95b91295f4ed0
Boy <abc35100762@gmail.com>
2026-01-08 19:49:46 +0800

    Time: 0 ms (100%), Space: 8 MB (97.96%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> rank(26, 100);
+        
+        for (int i = 0; i < order.size(); i++) {
+            rank[order[i] - 'a'] = i; 
+        }
+
+        sort(s.begin(), s.end(), [&](char a, char b) {
+            return rank[a - 'a'] < rank[b - 'a'];
+        });
+
+        return s;
+    }
+};
