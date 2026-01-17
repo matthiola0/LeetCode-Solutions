ad605c80a0dcc36836f424e4d98a06c0d59b82a9
Boy <abc35100762@gmail.com>
2026-01-17 11:45:28 +0800

    Time: 111 ms (45.04%), Space: 59.4 MB (56.2%) - LeetHub

---
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
+        int maxSide = 0;
+        int n = bottomLeft.size();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int minX = max(bottomLeft[i][0], bottomLeft[j][0]);
+                int minY = max(bottomLeft[i][1], bottomLeft[j][1]);
+                
+                int maxX = min(topRight[i][0], topRight[j][0]);
+                int maxY = min(topRight[i][1], topRight[j][1]);
+
+                if (minX < maxX && minY < maxY) {
+                    int width = maxX - minX;
+                    int height = maxY - minY;
+                    
+                    int len = min(width, height);
+                    maxSide = max(maxSide, len);
+                }
+            }
+        }
+
+        return 1LL * maxSide * maxSide;
+    }
+};
